956373fe3e926cc735be752c95b84ea474df3a38
ZiganshinIB <elzig3012@gmail.com>
2024-04-18 12:16:13 +0300

    Add models in AdminPanel

--- journey/admin.py
+++ journey/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from . models import Place, ImagePlace
 # Register your models here.
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_published')
+    list_filter = ('is_published', 'created_at', 'updated_at')
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description')
+    ordering = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(ImagePlace)
+class ImagePlaceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'place')
+    list_filter = ('place', 'created_at', 'updated_at')
+    search_fields = ('title', 'place__title')
+    ordering = ('place__title', 'title',)
