8854ae6f443fe9a1650f31366705152fc5bfa760
ZiganshinIB <elzig3012@gmail.com>
2024-04-18 11:26:21 +0300

    add Model Place

--- journey/models.py
+++ journey/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
+
+class PlaceManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_published=True)
+
+
+class Place(models.Model):
+    objects = PlaceManager()
+    title = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    lng = models.FloatField(verbose_name='Долгота')
+    lat = models.FloatField(verbose_name='Широта')
+    slug = models.SlugField(unique=True)
+    is_published = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('slug',)
+        ordering = ['title']
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+
+    # def get_absolute_url(self):
+    #     return reverse('place', kwargs={'slug': self.slug})
+
