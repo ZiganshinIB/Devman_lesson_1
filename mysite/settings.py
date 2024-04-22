0fc36eaa5ae3a5621f9bb50ca68029df8e859517
ZiganshinIB <elzig3012@gmail.com>
2024-04-22 19:17:28 +0300

    Add app tinymce https://github.com/jazzband/django-tinymce

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'adminsortable2',
     'journey.apps.JourneyConfig',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
--- mysite/urls.py
+++ mysite/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('journey.urls', namespace='journey')),
+    path('tinymce/', include('tinymce.urls')),
 ]
