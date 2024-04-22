35957f7f3421b3ac70ae970df0d69d012291d5e1
ZiganshinIB <elzig3012@gmail.com>
2024-04-22 12:53:51 +0300

    Add Fild 'Get Preview'

--- journey/admin.py
+++ journey/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 from . models import Place, ImagePlace
 # Register your models here.
 
 
-class ImagePlaceInline(admin.StackedInline):
+class ImagePlaceInline(admin.TabularInline):
+    readonly_fields = ('get_preview',)
     model = ImagePlace
+    def get_preview(self, obj):
+        return mark_safe('<img src="{url}" width="200" />'.format(url = obj.image.url, width=100, height=100))
+    fields = ('title', 'image', 'get_preview', 'position')
+    ordering = ('position',)
 
 
 @admin.register(Place)
@@ -21,6 +28,11 @@ class PlaceAdmin(admin.ModelAdmin):
 
 @admin.register(ImagePlace)
 class ImagePlaceAdmin(admin.ModelAdmin):
+    readonly_fields = ('get_image',)
+
+    def get_image(self, obj):
+        return format_html('<img src="{url}" height="200"/> '.format(url = obj.image.url, ))
+
     list_display_links = ('title',)
     list_display = ('title', 'place', 'position')
     list_filter = ('place', 'created_at', 'updated_at')
