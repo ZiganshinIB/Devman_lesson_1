d6a8a44641d7ea189692d78c97b1f059224c5c2a
ZiganshinIB <elzig3012@gmail.com>
2024-04-25 01:41:39 +0300

    Refract code

--- journey/models.py
+++ journey/models.py
@@ -3,12 +3,19 @@ from django.db import models
 from tinymce.models import HTMLField
 
 
+class BaseTimeModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
 class PlaceManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(is_published=True)
 
 
-class Place(models.Model):
+class Place(BaseTimeModel):
     objects = PlaceManager()
     place_id = models.CharField(max_length=100, db_index=True, unique=False, verbose_name='ID места')
     title = models.CharField(max_length=100, db_index=True, verbose_name='Название')
@@ -17,8 +24,6 @@ class Place(models.Model):
     lng = models.FloatField(verbose_name='Долгота')
     lat = models.FloatField(verbose_name='Широта')
     is_published = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['title']
@@ -29,18 +34,15 @@ class Place(models.Model):
         return self.title
 
 
-class ImagePlace(models.Model):
+class ImagePlace(BaseTimeModel):
     image = models.ImageField(verbose_name='Изображение')
     place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images', blank=False, null=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
     position = models.PositiveIntegerField(verbose_name='Позиция', default=0, blank=False, null=False)
 
     class Meta:
         verbose_name = 'Изображение места'
         verbose_name_plural = 'Изображения мест'
         ordering = ['position']
-        # unique_together = ('place', 'position')
 
     def __str__(self):
         return self.image.name
